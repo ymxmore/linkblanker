fc2e7a5f75abe1fda25c80b7222f8bb6ff452e61
ym-aozora <y_morita@aozora-create.com>
2015-11-10 01:09:09 +0900

    一旦スタイルを無効化

--- src/js/components/Tab.jsx
+++ src/js/components/Tab.jsx
@@ -10,6 +10,8 @@ var MaterialUi = require('material-ui');
 var React = window.React = require('react');
 var TabStore = require('../stores/Tab');
 
+var IconButton = MaterialUi.IconButton;
+
 var Tab = React.createClass({
   getInitialState: function () {
     return {
@@ -44,41 +46,38 @@ var Tab = React.createClass({
   },
 
   _getTabList: function (item, i) {
-    var children = '';
-
-    if (item.children.length > 0) {
-      children = item.children.map(this._getTabList);
-    }
-
     return (
-      <div className="tab-tree-group" key={i}>
-        <i
-          className="remove-children"
-          onClick={this._onClickRemoveChildren}/>
-        <div className="tab-tree">
-          <div className="tab-icon">
-            <Image
-              className="favicon"
-              src={item.info.favIconUrl}
-              url={item.info.url}
-              width="16"
-              height="16"
-              tabStatus={item.info.status || 'complete'}
-              alt={item.info.title}/>
-            <i
-              className="fa fa-times remove-tab"
-              onClick={this._onClickRemoveTab}/>
-          </div>
-          <div
-            className="tab"
-            key={item.info.id}
-            data-id={item.info.id}
-            title={item.info.title || item.info.url}
-            onClick={this._onClickTab}>
-            <h6>{item.info.title || item.info.url}</h6>
-          </div>
-          {children}
+      <div className="tab-tree">
+        <div className="tab-tree-icon">
+          <IconButton
+            iconClassName="fa fa-chevron-right"
+            onClick={this._onClickRemoveChildren}/>
+          <IconButton
+            iconClassName="fa fa-times"
+            onClick={this._onClickRemoveChildren}/>
         </div>
+        <div className="tab-icon">
+          <Image
+            className="favicon"
+            src={item.info.favIconUrl}
+            url={item.info.url}
+            width="16"
+            height="16"
+            tabStatus={item.info.status || 'complete'}
+            alt={item.info.title}/>
+          <IconButton
+            iconClassName="fa fa-times remove-tab"
+            onClick={this._onClickRemoveTab}/>
+        </div>
+        <div
+          className="tab"
+          key={item.info.id}
+          data-id={item.info.id}
+          title={item.info.title || item.info.url}
+          onClick={this._onClickTab}>
+          <h6>{item.info.title || item.info.url}</h6>
+        </div>
+        {item.children.map(this._getTabList)}
       </div>
     );
   },
